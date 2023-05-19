d27000b0a8c7684ec998aa73ab678fbd24bd6154
Reksa Panduasa <panduasareksa@students.amikom.ac.id>
2023-05-19 21:17:24 +0700

    Nambahi content tapi bingung

--- src/components/content/ProfileContent.jsx
+++ src/components/content/ProfileContent.jsx
@@ -1,8 +1,14 @@
-import "./content.css";
+import "./ProfileContent.css"
 
 function ProfileContent() {
   return (
-    <div className="content-dev">ProfileContent</div>
+    <section id="profileAnim">
+      <img src="/src/assets/kny.jpg" alt="" />
+      <div className="animeProfile">
+        <h3 className="judul">Kimetsu No Yaiba</h3>
+        <p className="sinopsis">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe maxime neque, omnis beatae, consectetur, minima rem aliquid corporis ipsa nisi eveniet esse vero iure blanditiis dolor quasi aperiam. Totam, illum. Atque enim eum optio consequatur mollitia quidem aliquam non culpa a vero soluta, quas alias corporis aut facilis libero, obcaecati, iure nemo assumenda? Quia ullam, accusantium nihil optio magnam temporibus quae nam fugiat iure rerum. Excepturi fugit explicabo est natus amet sequi quibusdam quas porro cumque quod, alias dicta minus obcaecati rerum doloribus. Laboriosam commodi, eaque cupiditate, ducimus adipisci inventore amet nihil consequuntur ad quia sequi voluptatibus. Nesciunt, quam hic.</p>
+        </div>
+    </section>
   )
 }
 
