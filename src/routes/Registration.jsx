1c80cc963cbdd10d30000e2bda65b0fdf09a6098
fakaha <lunastra34@gmail.com>
2023-05-30 18:13:31 +0700

    add: register page

--- src/routes/Registration.jsx
+++ src/routes/Registration.jsx
@@ -1,7 +1,36 @@
-
+import "../components/Registration.css"
 
 export const Registration = () => {
   return (
-    <div>Registration</div>
-  )
-}
+    <>
+      <div className="register">
+        <form className="registerForm">
+          <h2>Registrasi ANIMU</h2>
+          <div className="fill">
+            <input type="text" name="email" placeholder="Email" />
+            <input type="password" name="password" placeholder="Password" />
+            <input type="password" name="password" placeholder="Konfirmasi Password" />
+            <div className="verification">
+              <input type="text" placeholder="Kode Verifikasi"/>
+              <button>Kirim Kode</button>
+            </div>
+          </div>
+          <button className="registerButton" type="submit">
+            Registrasi
+          </button>
+          <p>
+          Sudah ada akun? <a href="">Login</a>
+          </p>
+          <div className="cekPrivasi">
+            <input type="checkbox" name="check" />
+            <p className="cekKeterangan">
+              Saya telah berusia 13 tahun ke atas. Dengan login, Anda telah
+              mengetahui dan menyetujui bahwa Anda telah membaca dan menyetujui
+              untuk terikat dengan Syarat Layanan dan Kebijakan Privasi
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
