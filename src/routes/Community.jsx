859f2231d44374edc8838b3b09efa18b8a5adf2e
fakaha <lunastra34@gmail.com>
2023-05-31 01:41:05 +0700

    add: community 40% on progress

--- src/routes/Community.jsx
+++ src/routes/Community.jsx
@@ -1,5 +1,7 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import searchIcon from "/search.svg";
+import "../components/community.css";
 
 export const Community = () => {
   return (
@@ -7,11 +9,26 @@ export const Community = () => {
     <Header/>
     <section className="container">
       <div className="content">
-        
+        <div className="headingContent">
+          <p>Welcome to Animu Discussions</p>
+          <p>Kalian diundang! <span>Forum diskusi Animu bisa digunakan untuk semua orang.</span></p>
+          <p>Cari dulu! <span>Sebelum kalian post pertanyaan atau topik pembicaraan kalian bisa searching dulu apakah pertanyaan atau topik pembicaraan kalian sudah ada atau belum</span></p>
+        </div>
+
+        <div className="mainContent">
+          <div className="search">
+            <input type="text" placeholder="Search discussion" className="searchBar"/>
+            <button>
+              <img src={searchIcon} alt="" />
+            </button>
+          </div>
+          <h3>Animu Forum Discussions</h3>
+        </div>
       </div>
 
       <aside>
-
+        <p>INFO!</p>
+        <p>Dikarenakan ini adalah website pertama kami maka dari itu untuk konten pada komunitas hanya sekedar forum. Nantikan fitur lainnya pada update berikutnya.!</p>
       </aside>
     </section>
     <Footer/>
