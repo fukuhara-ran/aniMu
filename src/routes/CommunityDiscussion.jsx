9df10547940b84e9a63515d5ba6f0908e8421726
fakaha <lunastra34@gmail.com>
2023-06-06 11:28:24 +0700

    make communityDiscussion jsx file

---
+++ src/routes/CommunityDiscussion.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+export const CommunityDiscussion = () => {
+    return (
+        <>
+        <Header/>
+        <div>
+            <b>INI PAGE Community Discussion</b>
+        </div>
+        <Footer/>
+        </>
+      )
+}
