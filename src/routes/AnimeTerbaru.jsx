ee1e88358b9cec2592c9ff4c4b475acb95fe44f9
kennn22 <kenedifp@gmail.com>
2023-06-07 15:30:28 +0700

    update animeTerbaru and genre

--- src/routes/AnimeTerbaru.jsx
+++ src/routes/AnimeTerbaru.jsx
@@ -1,12 +1,51 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import { Link } from "react-router-dom"
+import "../components/AnimeTerbaru.css"
 
 export const AnimeTerbaru = () => {
   return (
     <>
     <Header/>
-    <div>
-        <b>INI PAGE ANIME TERBARU</b>
+    <div id="container">
+        <div className="headingNewAnime">
+          <h4>Anime Terbaru</h4>
+        </div>
+
+        <div className="containerNewAnime">
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+          <div className="animeList">
+            <img src="/src/assets/kny.jpg" alt="" />
+            <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+          </div>
+        </div>
     </div>
     <Footer/>
     </>
--- src/routes/Genre.jsx
+++ src/routes/Genre.jsx
@@ -1,12 +1,39 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import { Link } from "react-router-dom"
+import arrow from "/src/Assets/arrow.svg";
+import "../components/genre.css"
 
 export const Genre = () => {
   return (
     <>
     <Header/>
-    <div>
-        <b>INI PAGE GENRE</b>
+    <div className="backGenre">
+      <div className="containerGenre">
+        <div className="headerGenre">
+          Genre
+        </div>
+        <div className="listGenre">
+            <div className="column">
+                <li><a href=""><img src={arrow}/><Link link to='/'>action</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Horror</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Mysteri</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Adventure</Link></a></li>
+            </div>
+            <div className="column">
+                <li><a href=""><img src={arrow}/><Link link to=''>Shounen</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Mecha</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Fantasy</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Slice of life</Link></a></li>
+            </div>
+            <div className="column">
+                <li><a href=""><img src={arrow}/><Link link to=''>Isekai</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Romance</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Drama</Link></a></li>
+                <li><a href=""><img src={arrow}/><Link link to=''>Comedy</Link></a></li>
+            </div>
+          </div>
+      </div>
     </div>
     <Footer/>
     </>
