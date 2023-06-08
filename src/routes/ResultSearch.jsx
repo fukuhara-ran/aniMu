d54c5fce5572e44b17432b7d14c674cd7e86fdca
FadhlurRahman11 <nurfadhlurrahman@students.amikom.ac.id>
2023-06-08 07:21:11 +0700

    update Result Searchin

--- src/routes/ResultSearch.jsx
+++ src/routes/ResultSearch.jsx
@@ -1,14 +1,48 @@
+import "../components/ResultSearch.css"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import { Link } from "react-router-dom";
 
 export const ResultSearch = () => {
     return (
         <>
         <Header/>
 
-        <div>
-            <h1>on progress........ :) </h1>
+        <div className="search-title">
+            <h1>Search Result for : <></></h1>
         </div>
+        <div className="search-items">
+            <div className="item">
+                <Link to='/profile-content'><img src="/src/assets/kny.jpg" alt="anime thumbnail"/></Link>
+                <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+            </div>
+
+            <div className="item">
+                <Link to='/profile-content'><img src="/src/assets/kny.jpg" alt="anime thumbnail"/></Link>
+                <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+            </div>
+
+            <div className="item">
+                <Link to='/profile-content'><img src="/src/assets/kny.jpg" alt="anime thumbnail"/></Link>
+                <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+            </div>
+
+            <div className="item">
+                <Link to='/profile-content'><img src="/src/assets/kny.jpg" alt="anime thumbnail"/></Link>
+                <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+            </div>
+
+            <div className="item">
+                <Link to='/profile-content'><img src="/src/assets/kny.jpg" alt="anime thumbnail"/></Link>
+                <h4><a href="#"><Link to=''>Judul Anime</Link></a></h4>
+            </div>
+            
+
+        </div>
+
+        <br />
+        <br />
+        <h1>CATATAN : DELAY PROGRESS BECAUSE ADA KELAS</h1>
 
         <Footer/>
         </>
