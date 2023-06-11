af2e1a0d2673ec4de750bb79d0368ff712aef8d7
FadhlurRahman11 <nurfadhlurrahman@students.amikom.ac.id>
2023-06-11 18:14:29 +0700

    update routes-help progress 85%

--- client/src/routes/Help.jsx
+++ client/src/routes/Help.jsx
@@ -1,4 +1,6 @@
 import logo from "../assets/ANIMU_RILL2.png";
+import logodiscussion from "../assets/discusion.png";
+import logostayuptodate from "../assets/stayuptodate.png";
 import "../components/Help.css";
 import Header from "../components/Header"
 import Footer from "../components/Footer"
@@ -21,22 +23,22 @@ export const Help = () => {
           <div className="items">
             <div className="first-item">
               <h3>STAY UP TO DATE</h3>
-              <img src="" alt="" />
+              <img src={logostayuptodate} alt="" />
               <p>Pantau terus dari anime kesayanganmu agar tidak ketinggalan berita-berita tentang anime terbaru</p>
             </div>
             <div className="second-item">
               <h3>JOIN THE DISCUSSION</h3>
-              <img src="" alt="" />
+              <img src={logodiscussion} alt="" />
               <p>Gabung diskusi bareng dengan pecinta anime lainnya bisa jadi kalian dapat teman baru di sana</p>
             </div>
           </div>
         </div>
         <div className="foot-Container">
-          <div>
+          <div className="head-text">
             <h1>Temukan lebih banyak tentang dunia anime sekarang</h1>
           </div>
           <div className="btn-daftar">
-            <button>Daftar disini</button>
+            <button className="btn-daftar"><h2>Daftar disini</h2></button>
           </div>
         </div>
     </div>
