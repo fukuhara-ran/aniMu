d26782fad20b898aea793255fd0178469463fce0
kennn22 <kenedifp@gmail.com>
2023-06-17 20:10:06 +0700

    create Start new discuss 40%

--- src/routes/StartDiscussions.jsx
+++ src/routes/StartDiscussions.jsx
@@ -1,18 +1,22 @@
 import Footer from "../components/Footer";
 import "../components/StartDiscussions.css";
 
-
 export const StartDiscussions = () => {
   return (
     <>
       <section className="containerCommunity">
         <div className="contentCommunity">
           <h1>Animu New Discussion</h1>
-            <div className="ContainerDiscussion">
-                <h4>START A NEW DISCUSSION</h4>
-
-                
+          <div className="ContainerDiscussion">
+            <h4>START A NEW DISCUSSION</h4>
+            <div className="boxDiscussion">
+              <div className="box-1"></div>
+              <div className="box-2"><input placeholder="enter a title" type="text" /></div>
+              <div className="box-3"></div>
+              <div className="box-4"><input placeholder="say something" type="text" /></div>
             </div>
+            <div className="postIn">hello</div>
+          </div>
         </div>
 
         <aside className="asideCommunity">
