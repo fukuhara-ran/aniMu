5662f06ea6dd008e69a87dce94786d7aba382d80
FadhlurRahman11 <nurfadhlurrahman@students.amikom.ac.id>
2023-06-18 10:16:35 +0700

    sudah aman route nya next ngoding desaign

--- src/routes/streams.jsx
+++ src/routes/streams.jsx
@@ -1,7 +1,7 @@
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import "../components/streaming.css";
-export const SearchResult = ({search, top}) => {
+export const Streams = ({}) => {
     return (
       <>
         
